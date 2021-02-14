1392dc3529825c08078d0dbb637ce72c0a73b88b
Stefan Surkamp <stefan.surkamp@netlight.com>
2021-02-14 22:52:57 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [text, setText] = useState(
+    "lel k asd aslel k asd aslel k asd aslel k asd aslel k asd aslel k asd aslel k asd aslel k asd aslel k asd aslel k asd aslel k asd aslel k asd aslel k asd asas asd asd asd asd asd asd asd asd a3rqefwl fjoiqwej fioqwjf iowjfiweojv dkjweiofjoi wjfoiw jfoiw3jf oiwjfoiwj foiwjfoi j32fioj 3owifjoiw3f jo3ijf oi3jf iow3jfoi3jf ioj3f io now im curious w ow ici hchqiow"
+  );
+  const [word, setWord] = useState("");
+
+  const textHasChanged = (e) => {
+    if (e.nativeEvent.data === " ") {
+      setText(text + e.target.value);
+      setWord("");
+      document.querySelector(".CompleteTextWrapper").scroll({
+        top: document.querySelector(".CompleteTextWrapper").scrollHeight,
+        behavior: "smooth",
+      });
+    } else {
+      setWord(e.target.value);
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="CompleteTextWrapper">
+        <div className="CompleteText">{text}</div>
+      </div>
+      <div className="WordInput">
+        <input type="text" onChange={textHasChanged} value={word} autofocus></input>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
